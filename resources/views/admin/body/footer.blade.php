7f07d23ffa0ade763a53cc6d5ac7038dbadc372f
loveikuto <hyw0305021@126.com>
2023-11-05 16:39:14 +1030

    Initial commit

---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between px-4 py-3 border-top small">
+				<p class="text-muted mb-1 mb-md-0">Copyright © 2023 <a href="https://www.vul-intel-plat.com" target="_blank">Vulnerability Intelligence Platform</a>.</p>
+				<p class="text-muted">Vulnerability Intelligence Platform With <i class="mb-1 text-primary ms-1 icon-sm" data-feather="heart"></i></p>
+			</footer>
---
+++ resources/views/admin/body/sidebar.blade.php
@@ -0,0 +1,153 @@
+<nav class="sidebar">
+      <div class="sidebar-header">
+        <a href="#" class="sidebar-brand">
+          V.I.<span>Platform</span>
+        </a>
+        <div class="sidebar-toggler not-active">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div class="sidebar-body">
+        <ul class="nav">
+          <li class="nav-item nav-category">Main</li>
+          <li class="nav-item">
+            <a href="{{ route('admin.dashboard') }}" class="nav-link">
+              <i class="link-icon" data-feather="box"></i>
+              <span class="link-title">Dashboard</span>
+            </a>
+          </li>
+          <li class="nav-item nav-category">Vulnerability Scan</li>
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#emails" role="button" aria-expanded="false" aria-controls="emails">
+              <i class="link-icon" data-feather="mail"></i>
+              <span class="link-title">Nessus</span>
+              <i class="link-arrow" data-feather="chevron-down"></i>
+            </a>
+            <div class="collapse" id="emails">
+              <ul class="nav sub-menu">
+                <li class="nav-item">
+                  <a href="pages/email/inbox.html" class="nav-link">Basic Scan</a>
+                </li>
+                <li class="nav-item">
+                  <a href="pages/email/read.html" class="nav-link">Advanced Scan</a>
+                </li>
+                <li class="nav-item">
+                  <a href="pages/email/compose.html" class="nav-link">Scan History</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+
+          <li class="nav-item">
+            <a href="pages/apps/calendar.html" class="nav-link">
+              <i class="link-icon" data-feather="calendar"></i>
+              <span class="link-title">Calendar</span>
+            </a>
+          </li>
+
+          <li class="nav-item nav-category">Threat Model</li>
+
+           <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#forms" role="button" aria-expanded="false" aria-controls="forms">
+              <i class="link-icon" data-feather="inbox"></i>
+              <span class="link-title">Forms</span>
+            </a>
+          </li>
+
+
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#tables" role="button" aria-expanded="false" aria-controls="tables">
+              <i class="link-icon" data-feather="layout"></i>
+              <span class="link-title">Table</span>
+              <i class="link-arrow" data-feather="chevron-down"></i>
+            </a>
+            <div class="collapse" id="tables">
+              <ul class="nav sub-menu">
+                <li class="nav-item">
+                  <a href="pages/tables/basic-table.html" class="nav-link">Basic Tables</a>
+                </li>
+                <li class="nav-item">
+                  <a href="pages/tables/data-table.html" class="nav-link">Data Table</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+
+          <li class="nav-item nav-category">General Report</li>
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#general-pages" role="button" aria-expanded="false" aria-controls="general-pages">
+              <i class="link-icon" data-feather="book"></i>
+              <span class="link-title">Risk Analysis</span>
+              <i class="link-arrow" data-feather="chevron-down"></i>
+            </a>
+            <div class="collapse" id="general-pages">
+              <ul class="nav sub-menu">
+                <li class="nav-item">
+                  <a href="pages/general/blank-page.html" class="nav-link">History</a>
+                </li>
+                
+              </ul>
+            </div>
+
+            <li class="nav-item">
+            <a class="nav-link"  data-bs-toggle="collapse" href="#charts" role="button" aria-expanded="false" aria-controls="charts">
+              <i class="link-icon" data-feather="pie-chart"></i>
+              <span class="link-title">Charts</span>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#forms" role="button" aria-expanded="false" aria-controls="forms">
+              <i class="link-icon" data-feather="inbox"></i>
+              <span class="link-title">Patching</span>
+            </a>
+          </li>
+
+          </li>
+          
+          <li class="nav-item nav-category">User Support</li>
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#authPages" role="button" aria-expanded="false" aria-controls="authPages">
+              <i class="link-icon" data-feather="unlock"></i>
+              <span class="link-title">Authentication</span>
+              <i class="link-arrow" data-feather="chevron-down"></i>
+            </a>
+            <div class="collapse" id="authPages">
+              <ul class="nav sub-menu">
+                <li class="nav-item">
+                  <a href="pages/auth/login.html" class="nav-link">Login</a>
+                </li>
+                <li class="nav-item">
+                  <a href="pages/auth/register.html" class="nav-link">Register</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#errorPages" role="button" aria-expanded="false" aria-controls="errorPages">
+              <i class="link-icon" data-feather="cloud-off"></i>
+              <span class="link-title">Error</span>
+              <i class="link-arrow" data-feather="chevron-down"></i>
+            </a>
+            <div class="collapse" id="errorPages">
+              <ul class="nav sub-menu">
+                <li class="nav-item">
+                  <a href="pages/error/404.html" class="nav-link">404</a>
+                </li>
+                <li class="nav-item">
+                  <a href="pages/error/500.html" class="nav-link">500</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a href="https://www.nobleui.com/html/documentation/docs.html" target="_blank" class="nav-link">
+              <i class="link-icon" data-feather="hash"></i>
+              <span class="link-title">Documentation</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
